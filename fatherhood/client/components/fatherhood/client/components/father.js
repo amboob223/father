45a137cb3ea8f49c96132628e3d8f4beb175e2f2
Abdul Mboob <67025021+amboob223@users.noreply.github.com>
2023-08-30 23:03:59 -0400

    Add files via upload

---
+++ fatherhood/client/components/fatherhood/client/components/father.js
@@ -0,0 +1,120 @@
+// does three things get the childs activities 
+// and randomly generates quotes for fathers
+//  and provides a get to to get the childs doctor
+
+ // to get info from the other file we just had to add another scripty tag in the father html 
+class Father {
+    constructor(name, age, style) {
+        this.name = name;
+        this.age = age;
+        this.style = style;
+
+        const ibtn = document.getElementById("gbtn");
+        ibtn.addEventListener("click", async (event) => {
+            event.preventDefault();
+            try {
+                const response = await fetch("http://localhost:5000/baby");
+                if (!response.ok) {
+                    throw new Error("Something went wrong");
+                }
+                const json = await response.json();
+
+                const inputName = prompt("Enter a name to search for:"); // the prompt becomes a string type thing where it can be accescepted
+                if (inputName) {
+                    await this.getInterest(inputName, json);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        });
+// this button gets a doctor 
+
+        const dbtn = document.getElementById("dbtn");
+                dbtn.addEventListener("click", async(event)=>{
+                    event.preventDefault();
+                    try {
+                        const response = await fetch("http://localhost:5000/baby");
+                            if(!response.ok){
+                                throw new Error("something wrong")
+                            }
+
+                            const json = await response.json();
+                            const inputBaby = prompt("what the baby name");
+
+                            if(inputBaby){
+                                await this.getDoctor(inputBaby,json)
+                            }
+                    } catch (error) {
+                        console.log(error)
+                    }
+                })
+
+                // this button gets random quotes 
+        const qbtn = document.getElementById("qbtn");
+                qbtn.addEventListener("click",async(event)=>{
+                    event.preventDefault();
+                    try {
+                        const div = document.getElementById("dip")
+                        const randomIndex = Math.floor(Math.random() * quotes.length)
+                        const randomQuote = quotes[randomIndex]
+                        div.innerHTML =randomQuote
+
+
+                    } catch (error) {
+                        console.log(error)
+                    }
+                })// here we need a random quote and i will get it from another page so I guess we will import it 
+
+    } // I think we have to put button events in the constructor this the end of the class
+
+    async getInterest(Bname, json) {
+        let nameFound = false;
+
+        for (let i = 0; i < json.rows.length; i++) {
+            const name = json.rows[i]["name"];
+            const intrest = json.rows[i]["intrest"];
+
+            if (name === Bname) {
+                console.log("Selected intrest:", intrest);
+
+                // Update your HTML here with the selected activity
+                // For example, you can use innerHTML to display the selected activity
+                // const activityDisplay = document.getElementById("activity-display");
+                // activityDisplay.innerHTML = intrest;
+
+                nameFound = true;
+                break; // Exit the loop since we found the matching name
+            }
+        }
+
+        if (!nameFound) {
+            console.log("Name not found");
+        }
+    }//methods
+
+    // this is how wee decalre a function 
+    async getDoctor (bname,json){
+        let nameFound = false;
+
+            for(let i= 0; i < json.rows.length; i++){
+                    const name = json.rows[i]["name"];
+                    const doctor = json.rows[i]["doctor"];
+
+                    if(name === bname){
+                        console.log("selected doctors",doctor)
+
+                        nameFound = true
+                        break;
+                    }
+
+
+
+            }
+            if(!nameFound){
+                console.log("name found")
+            }
+        }
+}
+
+// Create an instance of the Father class
+const fatherInstance = new Father("John", 40, "Casual");
---
+++ fatherhood/client/components/fatherhood/client/index.js
@@ -0,0 +1,85 @@
+
+
+// here is where i will keep the script for everything 
+        //father varibles
+    const fname = document.getElementById("fname");
+    const fage = document.getElementById("fage");
+    const fchild = document.getElementById("fchild");
+    const fbudget = document.getElementById("fbudget");
+    const fbtn = document.getElementById("fbtn");
+
+        //submit button 
+        fbtn.addEventListener("click", async(event)=>{
+            event.preventDefault()
+               
+                try {
+                     const body = {
+                    name:fname.value,
+                    age:fage.value,
+                    child:fchild.value,
+                    budget:fbudget.value,
+                }
+                const response = await fetch("http://localhost:5000/father",{
+                    method:"POST",    
+                    headers:{"Content-type":"application/json"},
+                    body:JSON.stringify(body)
+                })
+                alert("okay we got you pop")
+
+
+  
+                console.log("work")
+
+                fname.value =""
+                fage.value = ""
+                fchild.value = ""
+                fbudget.value = ""
+
+                } catch (error) {
+                    console.log(error)
+                }
+        })
+        //baby elememnts 
+    const bname = document.getElementById("bname");
+    const bage = document.getElementById("bage");
+    const bdoctor = document.getElementById("bdoctor");
+    const bintrest = document.getElementById("bintrest");
+    const bbtn = document.getElementById("bbtn");
+
+
+    //
+    bbtn.addEventListener("click", async (event)=>{
+
+            try {
+                event.preventDefault()
+
+                //here we using split and join to format the input for the array and put [] around it 
+                const val = bintrest.value.split(',')
+                const formatarr = `{${val.map(item => `'${item.trim()}'`).join(',')}}`
+                
+
+            const body= {
+                name:bname.value,
+                age:bage.value,
+                doctor:bdoctor.value,
+                intrest:formatarr
+            }
+            const response = await fetch("http://localhost:5000/baby",
+            {
+                method:"POST",
+                headers:{"Content-type":"Application/json"},
+                body:JSON.stringify(body)
+                
+            })
+            alert("okay we got you baby")
+            console.log("work")
+
+            bname.value =""
+            bage.value = ""
+            bdoctor.value = ""
+            bintrest.value = ""
+        
+            } catch (error) {
+                console.log(error)
+            }
+    })
